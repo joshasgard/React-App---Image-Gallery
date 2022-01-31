9c9e1c1aab153af9cc7fc5db871d4cba5e528261
joshasgard <idowuodesanmi@gmail.com>
2022-01-31 02:17:08 +0100

    feat: Add search component and controlled the content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
+
 
 const App = () => {
+  const [word, setWord] = useState(''); //using state hooks to make the search box user input available 
+                                        //as user is typing - called controlled component
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault(); //prevents default behaviour of the event, (i.e. default is to follow link on submit)
+    console.log(word);
+  }
   return (
     <div>
-      <Header title = "Image Search Gallery"/>  
+      <Header title = "Image Search Gallery"/> 
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+const navbarStyle = {
+    backgroundColor: '#F5F',
+};
 
 const Header = ({title}) => {
     return (
-        <Navbar bg="warning" variant="light">
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
-        </Navbar>
+        <Navbar style={navbarStyle} variant="light">
+        <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
     )
 };
 

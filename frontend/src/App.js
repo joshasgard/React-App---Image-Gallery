7d8cf506ea346eb19a60be815f835b7f85217413
joshasgard <idowuodesanmi@gmail.com>
2022-02-01 01:30:15 +0100

    feat: Add promise for unsplash api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,32 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState(''); //using state hooks to make the search box user input available 
-                                        //as user is typing - called controlled component
+  const [word, setWord] = useState(""); //using state hooks to make the search box user input available
+  //as user is typing - called controlled component
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault(); //prevents default behaviour of the event, (i.e. default is to follow link on submit)
+    e.preventDefault(); //prevents default behaviour of the event, (i.e. default to follow link on form submit)
     console.log(word);
+    fetch(`https://api.unsplash.com//photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((result) => result.json())
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
   }
+
   return (
     <div>
-      <Header title = "Image Search Gallery"/> 
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Image Search Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 export default App;
